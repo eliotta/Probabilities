3352b65a41b4f7a24d4b39f60abd0737dba2124f
Eric Liotta <eliotta@outlook.com>
2014-03-25 19:57:51 -0400

    fifth commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Probabilities::Application.config.secret_key_base = '97ea840b22ed0c931ecbf3a121226a18e5e5d30a83b2cdebe20d7d134d9ef35d55f7c7a889e4334b3bc544d9ff632aea99b78ac7814ca67bd77036d80403ae74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Probabilities::Application.config.session_store :cookie_store, key: '_Probabilities_session'
